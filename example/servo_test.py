2835df7824c9ab2cdaca412e385eb4589faa93e9
Hua Yanghao <yanghao@users.noreply.github.com>
2021-06-19 16:37:19 +0200

    Update servo_test.py

--- example/servo_test.py
+++ example/servo_test.py
@@ -1,12 +1,12 @@
 from microbit import pin1, sleep
 
-def servo_test(d=10, start=30, end=160):
-    for i in range(30, 160):
+def servo_test(d=10, start=30, end=160, step=10):
+    for i in range(start, end, step):
         pin1.write_analog(i)
         sleep(d)
-    for i in range(160, 30, -1):
+    for i in range(end, start, -step):
         pin1.write_analog(i)
         sleep(d)
 
 while True:
-    servo_test(20)
+    servo_test(100, 30, 160, 20)
