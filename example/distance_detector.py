59554e4286ffcbb34bc4c32aebeebd3040e9b46a
Hua Yanghao <yanghao@users.noreply.github.com>
2021-07-03 14:27:20 +0200

    Update distance_detector.py

--- example/distance_detector.py
+++ example/distance_detector.py
@@ -1,14 +1,20 @@
 # Note: code NOT tested, for Constantin to check out
 from tinybit import measure
 from utime import ticks_ms, ticks_diff
+import display
+from microbit import button_a
 
 D = 40 # 40 cm
 T = 3000 # 3 seconds -> 3000 ms
 start = None
 end = None
+N = 0
 
 while True:
     d = measure()
+    display.show(N)
+    if button_a.is_pressed():
+        N = 0
     if (d < D) and (start is None):
         start = ticks_ms()
     elif (d < D) and (start is not None): # time measurement started
@@ -16,8 +22,10 @@ while True:
         delta = ticks_diff(end, start)
         if delta > T:
             print("too close ....")
+            N = N + 1
             # start beep
     else:
         start = None
         end = None
         # stop beep
+     
