ae8b6779fcd41c72ab52120f30de0ba3fdc9673b
Hua Yanghao <yanghao@users.noreply.github.com>
2021-07-03 16:13:16 +0200

    Update noise_detector.py

--- example/noise_detector.py
+++ example/noise_detector.py
@@ -1,18 +1,16 @@
 # Add your Python code here. E.g.
-from microbit import display, sleep, pin1, pin2
+from microbit import pin1, pin2
 import neopixel as np
-from random import randint
 
 NUM = 24
 ring = np.NeoPixel(pin2, NUM)
 
 def noise(pin=pin1, num=NUM, max_num=1024):
     n = pin.read_analog()
-    step = max_num // 24
+    step = max_num // NUM
     m = n // step
-    assert m < 24
     for i in range(m):
-        ring[i] = (10, 0, 0)
+        ring[i] = (100, 0, 0)
     ring.show()
     ring.clear()
 
