2df22a51216faee03e2bbf0415050d07ba4691c1
Hua Yanghao <yanghao@users.noreply.github.com>
2021-07-10 14:36:53 +0200

    Update noise_detector.py

--- example/noise_detector.py
+++ example/noise_detector.py
@@ -9,8 +9,8 @@ def noise(pin=pin1, num=NUM, max_num=400):
     n = pin.read_analog()
     step = max_num // NUM
     m = n // step
-    if m >= NUM:
-        m = NUM - 1
+    if m > NUM:
+        m = NUM
     for i in range(m):
         ring[i] = (10, 0, 0)
     ring.show()
