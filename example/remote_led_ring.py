a39ee7c232a20bc622ae52e5c7dbe47a9bde4ffc
Hua Yanghao <yanghao@users.noreply.github.com>
2021-10-02 15:44:49 +0200

    Update remote_led_ring.py

--- example/remote_led_ring.py
+++ example/remote_led_ring.py
@@ -6,18 +6,21 @@ radio.on()
 NUM = 24
 ring = np.NeoPixel(pin2, NUM)
 
+# NOTE: microbit v1 does not support str().encode() and bytes().decode() methods.
+# So here it use str(b, 'utf-8') and bytes(s, 'utf-8') instead.
+
 def send(pin=pin0):
     display.show(Image.HEART)
     n = pin.read_analog()
     s = str(n)
-    b = s.encode()
-    radio.send(b)
+    b = bytes(s, 'utf-8')
+    radio.send(s)
 
 def receive(max_num=1020):
     display.show(Image.SMILE)
     try:
         b = radio.receive()
-        s = b.decode()
+        s = str(b, 'utf-8')
         n = int(s)
     except:
         return
